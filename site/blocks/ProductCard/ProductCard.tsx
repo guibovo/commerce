2738e44ed5456e556d73e3606dcf53d38441d407
Guilherme Fernandez <gui.bovo@hotmail.com>
2022-08-09 11:54:15 +0200

    :construction: Add Storefront client call to fetch data on builder and preview - need to be reworkd to create a hook to be able to switch betwen providers

--- site/blocks/ProductCard/ProductCard.tsx
+++ site/blocks/ProductCard/ProductCard.tsx
@@ -1,9 +1,73 @@
 import { Link } from '@components/ui'
+import { useEffect, useState } from 'react'
 import { Product } from '~/packages/commerce/types'
+import Client from 'shopify-buy';
 
-const ProductCard = ({ data }: { data: { product: Product }}) => {
+const ProductCard = ({ data, product_handle }: { data: { product: Product }, product_handle: string}) => {
+  const client = Client.buildClient({
+    domain: 'jvn-test.myshopify.com',
+    storefrontAccessToken: '45b3c40271fd0101fbcbc09efa3238c0'
+  });
+
+  const[product, setProduct] = useState(data?.product || null)
+
+  useEffect(() => {
+    if(!product) {
+      client.product.fetchByHandle(product_handle).then((res) => {
+        console.log(res);
+        setProduct(
+          {
+            id: res.id,
+            name: res.title,
+            description: res.description,
+            descriptionHtml: res.descriptionHtml,
+            sku: res.id,
+            slug: res.handle,
+            path: `/${res.handle}`,
+            images: [
+              {
+                  "url": res.images[0].src,
+                  "altText": res.images[0].altText,
+                  "width": res.images[0].height,
+                  "height": res.images[0].width
+              }
+          ],
+            variants: [
+              {
+                  "id": res.variants[0].id,
+                  "name": res.variants[0].title,
+                  "sku": res.variants[0].sku,
+                  "price": res.variants[0].price,
+                  "listPrice": res.variants[0].compaerAtPrice,
+                  "requiresShipping": true,
+                  "availableForSale": res.variants[0].availableForSale,
+                  "options": [
+                      {
+                          "__typename": "MultipleChoiceOption",
+                          "id": "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MDU4NTY1ODU5NzU1Mw==",
+                          "displayName": "title",
+                          "values": [
+                              {
+                                  "label": "Default Title"
+                              }
+                          ]
+                      }
+                  ]
+              }
+          ],
+            price: {
+              "value": 18,
+              "currencyCode": "USD"
+          },
+            options: [],
+            vendor: res.vendor
+          }
+        )
+
+      });
+    }
+  })
   
-  const product = data?.product
   return product 
     ? <div style={{ display: 'flex', flexDirection: 'column', maxWidth: '300px', alignContent: 'center', justifyContent: 'center' }}>
         <Link href={`/product${product.path}`}>
